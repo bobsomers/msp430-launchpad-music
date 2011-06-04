5ed03de4f26a31346026efc543fea8582543cdfa
Bob Somers <bob@bobsomers.com>
2011-06-04 06:32:19 -0700

    Done for now. Fun stuff.

--- launchpad-music.c
+++ launchpad-music.c
@@ -43,7 +43,10 @@
 
 // note definitions
 // (1,000,000 Hz / note frequency / 2)
+#define Fs3 2703
 #define G3 2551
+#define A3 2273
+#define B3 2025
 // TODO
 #define C4 1911
 #define Cs4 1804
@@ -192,8 +195,9 @@ interrupt(TIMERA1_VECTOR) timer_elapsed_isr() {
     }
 }
 
+/*
 // bob-omb battlefield theme from super mario 64
-void super_mario_64() {
+void bobomb_battlefield() {
     // measure 1
     play(C5, 2);
     play(A4, 2);
@@ -237,7 +241,219 @@ void super_mario_64() {
     play(A4, 4);
     rest(2);
     play(A4, 2);
+
+    // measure 5
+    play(G4, 1);
+    play(E4, 1);
+    play(G4, 1);
+    play(G5, 1);
+    rest(2);
+    play(G4, 1);
+    play(E4, 1);
+    play(G4, 1);
+    play(G5, 1);
+    rest(2);
+    play(G4, 1);
+    play(E4, 1);
+    play(G4, 1);
+    play(G5, 1);
+
+    // measure 6
+    rest(2);
+    play(G5, 2);
+    play(A5, 1);
+    play(G5, 2);
+    play(D5, 5);
+    rest(2);
+    play(D4, 1);
+    play(Eb4, 1);
+
+    // measure 7
+    play(E4, 1);
+    play(Eb4, 1);
+    play(E4, 1);
+    play(G4, 2);
+    play(A4, 1);
+    play(G4, 2);
+    play(Bb4, 4);
+    rest(2);
+    play(Ab4, 2);
+
+    // measure 8
+    play(A4, 1);
+    play(Ab4, 1);
+    play(A4, 1);
+    play(C5, 2);
+    play(D5, 1);
+    play(C5, 2);
+    play(A4, 4);
+    rest(2);
+    play(A4, 2);
+    
+    // measure 9
+    play(G4, 1);
+    play(Fs4, 1);
+    play(G4, 1);
+    play(G3, 1);
+    rest(2);
+    play(G4, 1);
+    play(Fs4, 1);
+    play(G4, 1);
+    play(G3, 1);
+    rest(1);
+    play(Eb4, 1);
+    play(E4, 2);
+    play(E4, 2);
+
+    // measure 10
+    play(C4, 12);
+    rest(4);
 }
+*/
+
+// theme from the princess slide in super mario 64
+void princess_slide() {
+    // measure 1
+    play(A5, 4);
+    play(A3, 4);
+    play(A5, 4);
+    play(A3, 4);
+
+    // measure 2
+    play(A5, 4);
+    play(Fs5, 4);
+    play(E5, 4);
+    play(Ds5, 4);
+
+    // measure 3
+    play(D5, 2);
+    play(G3, 2);
+    play(B3, 2);
+    play(G3, 2);
+    play(D5, 2);
+    play(G3, 2);
+    play(B3, 2);
+    play(G3, 2);
+
+    // measure 4
+    play(D5, 2);
+    play(G3, 2);
+    play(B3, 2);
+    play(G4, 1);
+    play(B4, 1);
+    play(D5, 2);
+    play(G3, 2);
+    play(E5, 2);
+    play(G3, 2);
+
+    // measure 5
+    play(D5, 2);
+    play(Fs3, 2);
+    play(A3, 2);
+    play(A4, 2);
+    play(Fs5, 2);
+    play(Fs3, 2);
+    play(A5, 2);
+    play(Fs3, 2);
+
+    // measure 6
+    play(Gs5, 2);
+    play(Fs3, 2);
+    play(A5, 2);
+    play(Fs3, 2);
+    play(Gs5, 1);
+    play(A5, 1);
+    play(Gs5, 1);
+    play(Fs5, 1);
+    play(D5, 2);
+    play(A4, 2);
+
+    // measure 7
+    play(B4, 2);
+    play(G3, 1);
+    play(Cs5, 1);
+    play(D5, 2);
+    play(G3, 1);
+    play(E5, 1);
+    play(Fs5, 1);
+    play(F5, 1);
+    play(Fs5, 1);
+    play(G5, 1);
+    play(A5, 1);
+    play(G5, 1);
+    play(Fs5, 1);
+    play(E5, 1);
+
+    // measure 8
+    play(D5, 1);
+    play(Cs5, 1);
+    play(D5, 1);
+    play(A5, 1);
+    play(Cs5, 1);
+    play(C5, 1);
+    play(Cs5, 1);
+    play(A5, 1);
+    play(Cs5, 1);
+    play(C5, 1);
+    play(B4, 1);
+    play(Bb4, 1);
+    play(A4, 1);
+    play(Gs4, 1);
+    play(G4, 1);
+    play(E4, 1);
+
+    // measure 9
+    play(A4, 2);
+    play(A3, 2);
+    play(D4, 2);
+    play(A3, 2);
+    play(B4, 2);
+    play(A3, 2);
+    play(A4, 2);
+    play(A3, 2);
+
+    // measure 10
+    play(D4, 2);
+    play(D5, 2);
+    play(Cs5, 2);
+    play(D5, 2);
+    play(Fs5, 2);
+    play(A3, 2);
+    play(E5, 2);
+    play(D5, 2);
+
+    // measure 11
+    play(B4, 2);
+    play(G3, 2); 
+    play(B4, 2);
+    play(G3, 2);
+    play(E5, 2);
+    play(G3, 2);
+    play(D5, 2);
+    play(G3, 2);
+
+    // measure 12
+    play(B4, 2);
+    play(B4, 2);
+    play(Bb4, 2);
+    play(B4, 2);
+    play(D5, 2);
+    play(G3, 2);
+    play(Cs5, 2);
+    play(B4, 2);
+
+
+    // measure 13
+    play(A4, 2);
+    play(Fs3, 2);
+    play(Fs3, 2);
+    play(A4, 2);
+    play(Fs3, 2);
+    play(Fs3, 2);
+    play(A5, 2);
+    play(Fs3, 2);
+}
+
 
 int main() {
     // initialize the system
@@ -253,11 +469,13 @@ int main() {
         // red led off
         LED_OUT &= ~LED_RED;
 
-        // set the bpm
-        set_bpm(110);
+        // play the bob-omb battlefield theme!
+        //set_bpm(110);
+        //bobomb_battlefield();
 
-        // play the song!
-        super_mario_64();
+        // play the princess slide theme!
+        set_bpm(180);
+        princess_slide();
     }
     
     return 0;

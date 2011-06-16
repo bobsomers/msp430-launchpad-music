e3b4f0354193f1d968d708b08223f7a14ac6d796
Sergio Campama <sergiocampama@gmail.com>
2011-06-16 17:31:51 -0400

    Add the Mario theme. It is currently not complete, because of memory issues (the song does not completely fit in 2kb.). Also, it is missing some final measures which I will finish some time later

--- launchpad-music.c
+++ launchpad-music.c
@@ -455,6 +455,222 @@ void princess_slide() {
 }
 
 
+void mario_theme_measure_1()
+{
+	play(E4, 1);
+	play(E4, 1);
+	rest(1);
+	play(E4, 1);
+	rest(1);
+	play(C4, 1);
+	play(E4, 1);
+	rest(1);
+	play(G4, 1);
+	rest(3);
+	play(G3, 1);
+	rest(3);
+}
+
+void mario_theme_measure_2()
+{
+	play(C4, 1);
+	rest(2);
+	play(G3, 1);
+	rest(2);
+	play(E3, 1);
+	rest(2);
+	play(A3, 1);
+	rest(1);
+	play(B3, 1);
+	rest(1);
+	play(As3, 1);
+	play(A3, 1);
+	rest(1);
+}
+
+void mario_theme_measure_3()
+{
+	play(G3, 1);
+	play(E4, 1);
+	rest(1);
+	play(G4, 1);
+	play(A4, 1);
+	rest(1);
+	play(F4, 1);
+	play(G4, 1);
+	rest(1);
+	play(E4, 1);
+	rest(1);
+	play(C4, 1);
+	play(D4, 1);
+	play(B3, 1);
+	rest(2);
+}
+
+void mario_theme_measure_4()
+{
+	rest(2);
+	play(G4, 1);
+	play(Fs4, 1);
+	play(F4,1);
+	play(Ds4, 1);
+	rest(1);
+	play(E4, 1);
+	rest(1);
+	play(Gs3, 1);
+	play(A3, 1);
+	play(C4, 1);
+	rest(1);
+	play(A3, 1);
+	play(C4, 1);
+	play(D4, 1);
+}
+
+void mario_theme_measure_5()
+{
+	rest(2);
+	play(G4, 1);
+	play(Fs4, 1);
+	play(F4,1);
+	play(Ds4, 1);
+	rest(1);
+	play(E4, 1);
+	rest(1);
+	play(C5, 1);
+	rest(1);
+	play(C5, 1);
+	play(C5, 1);
+	rest(3);
+}
+
+void mario_theme_measure_6()
+{
+	rest(2);
+	play(Eb4, 1);
+	rest(2);
+	play(D4, 1);
+	rest(2);
+	play(C4, 1);
+	rest(7);
+}
+
+void mario_theme_measure_7()
+{
+	play(C4, 1);
+	play(C4, 1);
+	rest(1);
+	play(C4, 1);
+	rest(1);
+	play(C4, 1);
+	play(D4, 1);
+	rest(1);
+	play(E4, 1);
+	play(C4, 1);
+	rest(1);
+	play(A3, 1);
+	play(G3, 1);
+	rest(3);
+}
+
+void mario_theme_measure_8()
+{
+	play(C4, 1);
+	play(C4, 1);
+	rest(1);
+	play(C4, 1);
+	rest(1);
+	play(C4, 1);
+	play(D4, 1);
+	play(E4,1);
+	rest(7);
+}
+
+/*void mario_theme_measure_9()
+{
+	play(E4, 1);
+	play(C4, 1);
+	rest(1);
+	play(G3, 1);
+	rest(2);
+	play(Gs3, 1);
+	rest(1);
+	play(A3, 1);
+	play(F4, 1);
+	rest(1);
+	play(F4, 1);
+	play(A3, 1);
+	rest(3);
+}
+
+void mario_theme_measure_10()
+{
+	play(B3, 1);
+	play(A4, 1);
+	rest(1);
+	play(A4, 1);
+	play(A4, 1);
+	play(G4, 1);
+	rest(1);
+	play(F4, 1);
+	play(E4, 1);
+	play(C4, 1);
+	rest(1);
+	play(A3, 1);
+	play(G3, 1);
+	rest(3);
+}
+
+void mario_theme_measure_11()
+{
+	play(B3, 1);
+	play(F4, 1);
+	rest(1);
+	play(F4, 1);
+	play(F4, 1);
+	play(E4, 1);
+	rest(1);
+	play(D4, 1);
+	play(C4, 1);
+	rest(7);
+	
+}*/
+
+void mario_theme()
+{
+	
+	mario_theme_measure_1();
+	mario_theme_measure_2();
+	mario_theme_measure_3();
+	mario_theme_measure_2();
+	mario_theme_measure_3();
+	mario_theme_measure_4();
+	mario_theme_measure_5();
+	mario_theme_measure_4();
+	mario_theme_measure_6();
+	mario_theme_measure_4();
+	mario_theme_measure_5();
+	mario_theme_measure_4();
+	mario_theme_measure_6();
+	mario_theme_measure_7();
+	mario_theme_measure_8();
+	mario_theme_measure_7();
+	mario_theme_measure_1();
+	mario_theme_measure_2();
+	mario_theme_measure_3();
+	mario_theme_measure_2();
+	mario_theme_measure_3();
+	/*mario_theme_measure_9();
+	mario_theme_measure_10();
+	mario_theme_measure_9();
+	mario_theme_measure_11();
+	mario_theme_measure_9();
+	mario_theme_measure_10();
+	mario_theme_measure_9();
+	mario_theme_measure_11();*/
+	
+}
+
+
 int main() {
     // initialize the system
     init();
@@ -474,8 +690,14 @@ int main() {
         //bobomb_battlefield();
 
         // play the princess slide theme!
-        set_bpm(180);
-        princess_slide();
+        //set_bpm(180);
+        //princess_slide();
+		
+		//play the mario theme!
+		//measures 9 to 11 are commented out because of memory issues
+		//but they are functional on their own
+		set_bpm(110);
+		mario_theme();
     }
     
     return 0;
